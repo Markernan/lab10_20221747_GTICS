d8fafe6b1ddf9dad24af0cfde5291feb21a55a6f
Jair <a20223806@pucp.edu.pe>
2025-11-13 21:25:36 -0500

    subida

---
+++ src/main/resources/templates/sql/mydb.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`agente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`agente` (
+                                               `id` VARCHAR(255) NOT NULL,
+    `nombre_completo` TEXT(255) NULL,
+    `carrera` TEXT(255) NULL,
+    `universidad` TEXT(255) NULL,
+    `email` TEXT(255) NULL,
+    `pais` TEXT(255) NULL,
+    `estado` TEXT(255) NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
